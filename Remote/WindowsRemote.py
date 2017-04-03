278df8268fd75c083cb88dc1f85e3d69014a5c8b
Andrey Dyachenko <winnts@gmail.com>
2017-04-03 08:58:04 +0300

    refactoring

--- Remote/WindowsRemote.py
+++ Remote/WindowsRemote.py
@@ -5,56 +5,47 @@ from winrm.protocol import Protocol
 import os
 
 
-def download_msi(link):
-    p = Protocol(
-        endpoint='http://CIT10-WIN7-64:5985/wsman',
-        transport='ntlm',
-        username=r'admin',
-        password='test',
-        server_cert_validation='ignore')
-    shell_id = p.open_shell()
-    command_id = p.run_command(shell_id, 'wget', ['--content-disposition', '--no-check-certificate', link])
-    std_out, std_err, status_code = p.get_command_output(shell_id, command_id)
-    p.cleanup_command(shell_id, command_id)
-    print(std_out, status_code)
-    p.close_shell(shell_id)
+class WindowsRemote(object):
+    def protocol(self, endpoint, username, passwd):
+        return Protocol(
+            endpoint='http://' + endpoint + ':5985/wsman',
+            transport='ntlm',
+            username=username,
+            password=passwd,
+            server_cert_validation='ignore')
+
+    def download_msi(self, link, protocol):
+        shell_id = protocol.open_shell()
+        command_id = protocol.run_command(shell_id, 'wget', ['--content-disposition', '--no-check-certificate', link])
+        std_out, std_err, status_code = protocol.get_command_output(shell_id, command_id)
+        protocol.cleanup_command(shell_id, command_id)
+        print(std_out, status_code)
+        protocol.close_shell(shell_id)
 
 
-def install_agent(filename):
-    p = Protocol(
-        endpoint='http://CIT10-WIN7-64:5985/wsman',
-        transport='ntlm',
-        username=r'admin',
-        password='test',
-        server_cert_validation='ignore')
-    shell_id = p.open_shell()
-    command_id = p.run_command(shell_id, filename, ['/q'])
-    std_out, std_err, status_code = p.get_command_output(shell_id, command_id)
-    p.cleanup_command(shell_id, command_id)
-    print(std_out, status_code)
-    p.close_shell(shell_id)
+    def install_agent(self, protocol, filename):
+        shell_id = protocol.open_shell()
+        command_id = protocol.run_command(shell_id, filename, ['/q'])
+        std_out, std_err, status_code = protocol.get_command_output(shell_id, command_id)
+        protocol.cleanup_command(shell_id, command_id)
+        print(std_out, status_code)
+        protocol.close_shell(shell_id)
 
 
-def find_msi():
-    p = Protocol(
-        endpoint='http://CIT10-WIN7-64:5985/wsman',
-        transport='ntlm',
-        username=r'admin',
-        password='test',
-        server_cert_validation='ignore')
-    shell_id = p.open_shell()
-    command_id = p.run_command(shell_id, 'dir', ['itsm*.msi', '/s', '/b'])
-    std_out, std_err, status_code = p.get_command_output(shell_id, command_id)
-    p.cleanup_command(shell_id, command_id)
-    print(std_out, status_code)
-    p.close_shell(shell_id)
-    return std_out
+    def find_msi(self, protocol):
+        shell_id = protocol.open_shell()
+        command_id = protocol.run_command(shell_id, 'dir', ['itsm*.msi', '/s', '/b'])
+        std_out, std_err, status_code = protocol.get_command_output(shell_id, command_id)
+        protocol.cleanup_command(shell_id, command_id)
+        print(std_out, status_code)
+        protocol.close_shell(shell_id)
+        return std_out
 
 
-
-download_msi('https://robotauto-msp.itsm-cit.comodo.com:443/enroll/windows/msi/token/107d9a6fb63ff7022acb45f49465c57a')
-msi_name = find_msi()
+link = 'https://robotauto-msp.itsm-cit.comodo.com:443/enroll/windows/msi/token/107d9a6fb63ff7022acb45f49465c57a'
+remote = WindowsRemote()
+p = remote.protocol()
+remote.download_msi(p, link)
+msi_name = remote.find_msi(p)
 print msi_name
-install_agent(msi_name)
-
-# connect_and_run()
+remote.install_agent(p, msi_name)
